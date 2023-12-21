ceed5b821f1be46539d038208efe7f6ba2359543
Eavelie <athallahnaufalr@gmail.com>
2023-12-21 22:28:19 +0700

    added tooltip in hadiah-list

--- script.js
+++ script.js
@@ -17,3 +17,6 @@ var interval = setInterval(function() {
 
 
 }, 1000);
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
